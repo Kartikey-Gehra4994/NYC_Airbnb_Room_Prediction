1ab65b0f3bef257f558477bffd37d72154779620
Kartikey-Gehra4994 <kartikgehra@gmail.com>
2026-09-17 12:50:47 +0530

    update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,7 +1,7 @@
 import os
 import urllib.request
 
-MODEL_URL = "https://github.com/Kartikey-Gehra4994/NYC_Airbnb_Room_Prediction/releases/tag/v1.0.0"
+MODEL_URL = "https://github.com/Kartikey-Gehra4994/NYC_Airbnb_Room_Prediction/releases/download/v1.0.0/model_pipeline.pkl"
 MODEL_PATH = "model_pipeline.pkl"
 
 if not os.path.exists(MODEL_PATH):
